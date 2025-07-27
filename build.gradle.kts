ff88900e3832b2363e3964f1fb43edcec6983e70
Josh <43449531+joshbker@users.noreply.github.com>
2025-07-27 10:51:14 +0100

    chore: upgrade discordgenerator to 1.0.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ dependencies {
     implementation("net.dv8tion:JDA:5.0.0-beta.15") {
         exclude(module = "opus-java")
     }
-    implementation("gg.flyte:discordgenerator:1.0.5")
+    implementation("gg.flyte:discordgenerator:1.0.7")
 }
 
 publishing {
